698620cb32229de4a3533c7d49278daadaeba238
rafiazharr17 <rafipilo17@gmail.com>
2024-12-18 17:04:47 +0700

    membuat dao untuk operasi insert dan read pada tabel dokter

---
+++ app/src/main/java/com/rafi/pertemuan9/data/dao/DokterDao.kt
@@ -0,0 +1,16 @@
+package com.rafi.pertemuan9.data.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.rafi.pertemuan9.data.entity.Dokter
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface DokterDao {
+    @Insert
+    suspend fun insertDokter(dokter: Dokter)
+
+    @Query("SELECT * FROM dokter ORDER BY namaDokter ASC")
+    fun getAllDokter(): Flow<List<Dokter>>
+}
