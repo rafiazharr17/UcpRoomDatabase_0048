403c86e5d6c946762389a62db1607294a8e30d21
rafiazharr17 <rafipilo17@gmail.com>
2024-12-18 16:59:25 +0700

    membuat entity untuk jadwal dengan foreignkey dokter

---
+++ app/src/main/java/com/rafi/pertemuan9/data/entity/Jadwal.kt
@@ -0,0 +1,26 @@
+package com.rafi.pertemuan9.data.entity
+
+import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "jadwal",
+    foreignKeys = [
+        ForeignKey(
+            entity = Dokter::class,
+            parentColumns = arrayOf("idDokter"),
+            childColumns = arrayOf("dokter"),
+            onDelete = ForeignKey.CASCADE
+        )
+    ]
+)
+data class Jadwal(
+    @PrimaryKey
+    val idJadwal: String,
+    val idDokter: String,
+    val namaPasien: String,
+    val noHpPasien: String,
+    val tanggal: String,
+    val status: String,
+)
