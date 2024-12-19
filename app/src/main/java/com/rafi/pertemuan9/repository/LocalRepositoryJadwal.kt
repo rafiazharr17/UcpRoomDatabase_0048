7cd8221a6ddee76f1caa926bff66b06ad2d1cd8a
rafiazharr17 <rafipilo17@gmail.com>
2024-12-19 23:05:10 +0700

    membuat LocalRepositoryJadwal yang berisi implementasi dari RepositoryJadwal yang menggunakan JadwalDao untuk mengelola data jadwal

---
+++ app/src/main/java/com/rafi/pertemuan9/repository/LocalRepositoryJadwal.kt
@@ -0,0 +1,29 @@
+package com.rafi.pertemuan9.repository
+
+import com.rafi.pertemuan9.data.dao.JadwalDao
+import com.rafi.pertemuan9.data.entity.Jadwal
+import kotlinx.coroutines.flow.Flow
+
+class LocalRepositoryJadwal(
+    private val jadwalDao: JadwalDao
+) : RepositoryJadwal {
+    override suspend fun insertJadwal(jadwal: Jadwal) {
+        jadwalDao.insertJadwal(jadwal)
+    }
+
+    override suspend fun deleteJadwal(jadwal: Jadwal) {
+        jadwalDao.deletejadwal(jadwal)
+    }
+
+    override suspend fun updateJadwal(jadwal: Jadwal) {
+        jadwalDao.updatejadwal(jadwal)
+    }
+
+    override fun getAllJadwal(): Flow<List<Jadwal>> {
+        return jadwalDao.getAlljadwal()
+    }
+
+    override fun getJadwal(idJadwal: Int): Flow<Jadwal> {
+        return jadwalDao.getJadwal(idJadwal)
+    }
+}
--- app/src/main/java/com/rafi/pertemuan9/repository/RepositoryJadwal.kt
+++ app/src/main/java/com/rafi/pertemuan9/repository/RepositoryJadwal.kt
@@ -1,6 +1,5 @@
 package com.rafi.pertemuan9.repository
 
-import com.rafi.pertemuan9.data.entity.Dokter
 import com.rafi.pertemuan9.data.entity.Jadwal
 import kotlinx.coroutines.flow.Flow
 
