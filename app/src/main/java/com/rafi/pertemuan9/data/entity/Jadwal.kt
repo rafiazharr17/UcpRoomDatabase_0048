bdf72ef2e557d57109b82914a640703f1198751f
rafiazharr17 <rafipilo17@gmail.com>
2024-12-19 22:33:40 +0700

    mengahapus foreignkey di tabel jadwal dan mengganti tipe data idJadwal menjadi int auto increment

--- app/src/main/java/com/rafi/pertemuan9/data/entity/Jadwal.kt
+++ app/src/main/java/com/rafi/pertemuan9/data/entity/Jadwal.kt
@@ -1,27 +1,12 @@
 package com.rafi.pertemuan9.data.entity
 
-import androidx.room.ColumnInfo
 import androidx.room.Entity
-import androidx.room.ForeignKey
-import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity(
-    tableName = "jadwal",
-    foreignKeys = [
-        ForeignKey(
-            entity = Dokter::class,
-            parentColumns = arrayOf("idDokter"),
-            childColumns = arrayOf("dokter"),
-            onDelete = ForeignKey.CASCADE
-        )
-    ],
-    indices = [Index(value = ["idDokter"])]
-)
+@Entity(tableName = "jadwal")
 data class Jadwal(
-    @PrimaryKey
-    val idJadwal: String,
-    @ColumnInfo(index = true)
+    @PrimaryKey(autoGenerate = true)
+    val idJadwal: Int = 0,
     val dokter: String,
     val namaPasien: String,
     val noHpPasien: String,
