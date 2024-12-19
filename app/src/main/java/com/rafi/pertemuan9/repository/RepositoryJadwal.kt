d21141544ab384b712d8a86c57c4b5ee5836ebfd
rafiazharr17 <rafipilo17@gmail.com>
2024-12-19 22:43:57 +0700

    membuat RepositoryJadwal untuk mendefinisikan fungsi-fungsi yang harus di implementasikan

---
+++ app/src/main/java/com/rafi/pertemuan9/repository/RepositoryJadwal.kt
@@ -0,0 +1,16 @@
+package com.rafi.pertemuan9.repository
+
+import com.rafi.pertemuan9.data.entity.Dokter
+import kotlinx.coroutines.flow.Flow
+
+interface RepositoryJadwal {
+    suspend fun insertJadwal(dokter: Dokter)
+
+    suspend fun deleteJadwal(dokter: Dokter)
+
+    suspend fun updateJadwal(dokter: Dokter)
+
+    fun getAllJadwal(): Flow<List<Dokter>>
+
+    fun getJadwal(nim: String): Flow<Dokter>
+}
