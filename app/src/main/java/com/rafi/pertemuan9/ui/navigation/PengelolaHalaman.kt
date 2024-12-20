c87b1ea5b08a85fc5b12c302156039942b8da9be
rafiazharr17 <rafipilo17@gmail.com>
2024-12-20 12:23:46 +0700

    membuat PengelolaHalaman untuk route navigasi agar lebih terstruktur

---
+++ app/src/main/java/com/rafi/pertemuan9/ui/navigation/PengelolaHalaman.kt
@@ -0,0 +1,47 @@
+package com.rafi.pertemuan9.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.rafi.pertemuan9.ui.view.dokter.HomeDokterView
+import com.rafi.pertemuan9.ui.view.dokter.InsertDokterView
+
+@Composable
+fun PengelolaHalaman(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHomeDokter.route
+    ) {
+        composable(
+            route = DestinasiHomeDokter.route
+        ) {
+            HomeDokterView(
+                onDetailClick = {},
+                onAddDokter = {
+                    navController.navigate(DestinasiInsertDokter.route)
+                },
+                modifier = modifier
+            )
+        }
+
+        composable (
+            route = DestinasiInsertDokter.route
+        ) {
+            InsertDokterView(
+                onBack = {
+                    navController.popBackStack()
+                },
+                onNavigate = {
+                    navController.popBackStack()
+                },
+                modifier = modifier
+            )
+        }
+    }
+}
