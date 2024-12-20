3e3a4b0172681f5d248bc0c4293501aac060a230
rafiazharr17 <rafipilo17@gmail.com>
2024-12-20 14:06:12 +0700

    menambahkan textfield untuk search dan garis horizontal, serta memperbaiki modifier pada Column dan Row

--- app/src/main/java/com/rafi/pertemuan9/ui/customwidget/TopAppBar.kt
+++ app/src/main/java/com/rafi/pertemuan9/ui/customwidget/TopAppBar.kt
@@ -8,14 +8,24 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -25,27 +35,29 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TopAppBar(
     onBack: () -> Unit,
     showBackButton: Boolean = true,
     showJadwalButton: Boolean = true,
     showTambahDokterButton: Boolean = true,
+    showSearch: Boolean = true,
     navigateJadwal: () -> Unit,
     navigateTambahDokter: () -> Unit,
     judul: String,
+    judulSearch: String,
     modifier: Modifier = Modifier
 ) {
+    var search by remember { mutableStateOf("") }
+
     Column(
         modifier = Modifier.fillMaxWidth()
-            .padding(top = 16.dp, bottom = 16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+            .padding(top = 16.dp, bottom = 16.dp)
     ) {
         Row(
             modifier = Modifier.fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Center
+            verticalAlignment = Alignment.CenterVertically
         ) {
             if (showBackButton) {
                 IconButton (onClick = onBack) {
@@ -57,6 +69,8 @@ fun TopAppBar(
                 }
             }
 
+            Spacer(modifier = Modifier.padding(start = 16.dp))
+
             Text(
                 text = judul,
                 fontWeight = FontWeight.Bold,
@@ -65,7 +79,33 @@ fun TopAppBar(
             )
         }
 
-        Spacer(modifier = Modifier.height(8.dp))
+        Spacer(modifier = Modifier.height(10.dp))
+
+        if (showSearch){
+            TextField(
+                value = search,
+                onValueChange = { search = it },
+                modifier = Modifier
+                    .fillMaxWidth(),
+                placeholder = { Text(text = judulSearch) },
+                singleLine = true,
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Search,
+                        contentDescription = "Search"
+                    )
+                },
+                colors = TextFieldDefaults.textFieldColors(
+                    containerColor = Color.LightGray,
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent
+                ),
+                shape = RoundedCornerShape(15.dp)
+            )
+        }
+
+
+        Spacer(modifier = Modifier.height(10.dp))
 
         Row(
             modifier = Modifier.fillMaxWidth(),
@@ -92,5 +132,12 @@ fun TopAppBar(
                 }
             }
         }
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        HorizontalDivider(
+            thickness = 5.dp,
+            color = Color.Black
+        )
     }
 }
