fa138989eab1bc74a3658bee78c54172bdc8908b
rafiazharr17 <rafipilo17@gmail.com>
2024-12-20 10:56:43 +0700

    menambahkan object untuk list data Spesialis

---
+++ app/src/main/java/com/rafi/pertemuan9/data/objek/PilihSpesialis.kt
@@ -0,0 +1,10 @@
+package com.rafi.pertemuan9.data.objek
+
+object PilihSpesialis {
+    val options = listOf(
+        "Spesialis Anak",
+        "Spesialis Bedah Saraf",
+        "Spesialis Akupuntur Medik",
+        "Spesialis Bedah"
+    )
+}
