ea8696a39e791342d4d334b85ded2230e06148d0
rafiazharr17 <rafipilo17@gmail.com>
2024-12-22 04:09:38 +0700

    membuat DetailJadwalView untuk tampilan detail data jadwal

---
+++ app/src/main/java/com/rafi/pertemuan9/ui/view/jadwal/DetailJadwalView.kt
@@ -0,0 +1,261 @@
+package com.rafi.pertemuan9.ui.view.jadwal
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.rafi.pertemuan9.data.entity.Jadwal
+import com.rafi.pertemuan9.ui.viewmodel.PenyediaViewModel
+import com.rafi.pertemuan9.ui.viewmodel.jadwal.DetailJadwalUiState
+import com.rafi.pertemuan9.ui.viewmodel.jadwal.DetailJadwalViewModel
+import com.rafi.pertemuan9.ui.viewmodel.jadwal.toJadwalEntity
+
+
+@Composable
+fun DetailJadwalView (
+    modifier: Modifier = Modifier,
+    viewModel: DetailJadwalViewModel = viewModel (factory = PenyediaViewModel.Factory),
+    onBack: () -> Unit = { },
+    onEditClick: (Int) -> Unit = { },
+    onDeleteClick: () -> Unit = { }
+){
+    Box(
+        modifier = Modifier
+            .background(color = Color(0xFF00AAEC))
+            .fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 10.dp)
+        ) {
+            Row(
+                modifier = modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                IconButton(onClick = onBack) {
+                    Icon(
+                        imageVector = Icons.Default.ArrowBack,
+                        contentDescription = "",
+                        modifier = Modifier.size(35.dp),
+                        tint = Color.White
+                    )
+                }
+
+                Text(
+                    text = "Detail Jadwal",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 25.sp,
+                    color = Color.White
+                )
+                Box {  }
+                Box {  }
+            }
+
+            Card(
+                modifier = Modifier.fillMaxSize()
+            ) {
+                val detailJadwalUiState by viewModel.detailJadwalUiState.collectAsState()
+
+                BodyDetailJadwal(
+                    detailJadwalUiState = detailJadwalUiState,
+                    onDeleteClick = {
+                        viewModel.deleteMhs()
+                        onDeleteClick()
+                    }
+                )
+            }
+        }
+
+        FloatingActionButton(
+            onClick = {
+                onEditClick(viewModel.detailJadwalUiState.value.detailJadwalUiEvent.idJadwal)
+            },
+            shape = MaterialTheme.shapes.medium,
+            modifier = modifier
+                .align(Alignment.BottomEnd)
+                .padding(16.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Default.Edit,
+                contentDescription = "Edit Jadwal",
+            )
+        }
+
+    }
+}
+
+
+@Composable
+fun BodyDetailJadwal (
+    modifier: Modifier = Modifier,
+    detailJadwalUiState: DetailJadwalUiState = DetailJadwalUiState(),
+    onDeleteClick: () -> Unit = { }
+) {
+    var deleteConfirmationRequired by rememberSaveable { mutableStateOf(false) }
+    when {
+        detailJadwalUiState.isLoading -> {
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator() // Tampilkan loading
+            }
+        }
+
+        detailJadwalUiState.isUiEventNotEmpty -> {
+            Column(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                ItemDetailJadwal(
+                    jadwal = detailJadwalUiState.detailJadwalUiEvent.toJadwalEntity(),
+                    modifier = Modifier
+                )
+                Spacer(modifier = Modifier.padding(8.dp))
+                Button (
+                    onClick = {
+                        deleteConfirmationRequired = true
+                    },
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Text(text = "Delete")
+                }
+                if (deleteConfirmationRequired) {
+                    DeleteConfirmationDialog(
+                        onDeleteConfirm = {
+                            deleteConfirmationRequired = false
+                            onDeleteClick()
+                        },
+                        onDeleteCancel = { deleteConfirmationRequired = false },
+                        modifier = Modifier.padding(8.dp)
+                    )
+                }
+            }
+        }
+
+        detailJadwalUiState.isUiEventEmpty -> {
+            Box(
+                modifier = modifier.fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Data tidak ditemukan",
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun ItemDetailJadwal (
+    modifier: Modifier = Modifier,
+    jadwal: Jadwal
+){
+    Card (
+        modifier = modifier
+            .fillMaxWidth (),
+        colors = CardDefaults.cardColors (
+            containerColor = MaterialTheme.colorScheme.primaryContainer,
+            contentColor = MaterialTheme.colorScheme.onPrimaryContainer
+        )
+    ){
+        Column (
+            modifier = Modifier.padding(16.dp)
+        ){
+            ComponentDetailJadwal (judul = "ID Jadwal", isinya = jadwal.idJadwal.toString())
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailJadwal (judul = "Nama Pasien", isinya = jadwal.namaPasien)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailJadwal (judul = "Nomor Handphone Pasien", isinya = jadwal.noHpPasien)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailJadwal (judul = "Nama Dokter", isinya = jadwal.dokter)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailJadwal (judul = "Tanggal", isinya = jadwal.tanggal)
+            Spacer(modifier = Modifier.padding(4.dp))
+            ComponentDetailJadwal (judul = "Status Penanganan", isinya = jadwal.status)
+        }
+    }
+}
+
+@Composable
+fun ComponentDetailJadwal (
+    modifier: Modifier = Modifier,
+    judul: String,
+    isinya: String,
+) {
+    Column (
+        modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.Start
+    ) {
+        Text(
+            text = "$judul : ",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Gray
+
+        )
+        Text(
+            text = isinya, fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+        )
+    }
+}
+
+@Composable
+private fun DeleteConfirmationDialog (
+    onDeleteConfirm: () -> Unit, onDeleteCancel: () -> Unit, modifier: Modifier =
+        Modifier
+) {
+    AlertDialog(onDismissRequest = { /* Do nothing */ },
+        title = { Text("Delete Data") },
+        text = { Text("Apakah anda yakin ingin menghapus data?") },
+        modifier = modifier,
+        dismissButton = {
+            TextButton (onClick = onDeleteCancel) {
+                Text(text = "Cancel")
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = onDeleteConfirm) {
+                Text(text = "Yes")
+            }
+        }
+    )
+}
