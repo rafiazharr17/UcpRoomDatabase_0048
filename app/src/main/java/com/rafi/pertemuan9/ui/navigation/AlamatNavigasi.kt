26596b97415e25a6b984948b591cbcdf51f916a8
rafiazharr17 <rafipilo17@gmail.com>
2024-12-22 04:08:23 +0700

    menambahkan navigasi untuk DetailJadwal dan UpdateJadwal

--- app/src/main/java/com/rafi/pertemuan9/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/rafi/pertemuan9/ui/navigation/AlamatNavigasi.kt
@@ -19,3 +19,15 @@ object DestinasiInsertJadwal : AlamatNavigasi {
 object DestinasiHomeJadwal : AlamatNavigasi {
     override val route = "home_jadwal"
 }
+
+object DestinasiDetailJadwal : AlamatNavigasi {
+    override val route = "detail"
+    const val IDJADWAL = "idJadwal"
+    val routeWithArg = "$route/{${IDJADWAL}}"
+}
+
+object DestinasiUpdateJadwal : AlamatNavigasi {
+    override val route = "update"
+    const val IDJADWAL = "idJadwal"
+    val routeWithArg = "$route/{$IDJADWAL}"
+}
