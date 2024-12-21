689d958772310b77c0401b79ef71ec421257480f
rafiazharr17 <rafipilo17@gmail.com>
2024-12-22 04:11:30 +0700

    menambahkan route ke DestinasiDetailJadwal dan DestinasiUpdateJadwal, tetapi untuk route ke DestinasiUpdateJadwal masih belum bisa

--- app/src/main/java/com/rafi/pertemuan9/ui/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/rafi/pertemuan9/ui/navigation/PengelolaHalaman.kt
@@ -3,13 +3,17 @@ package com.rafi.pertemuan9.ui.navigation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.rafi.pertemuan9.ui.view.dokter.HomeDokterView
 import com.rafi.pertemuan9.ui.view.dokter.InsertDokterView
+import com.rafi.pertemuan9.ui.view.jadwal.DetailJadwalView
 import com.rafi.pertemuan9.ui.view.jadwal.HomeJadwalView
 import com.rafi.pertemuan9.ui.view.jadwal.InsertJadwalView
+import com.rafi.pertemuan9.ui.view.jadwal.UpdateJadwalView
 
 @Composable
 fun PengelolaHalaman(
@@ -53,7 +57,10 @@ fun PengelolaHalaman(
             route = DestinasiHomeJadwal.route
         ) {
             HomeJadwalView(
-                onDetailClick = {},
+                onDetailClick = {idJadwal ->
+                    navController.navigate("${DestinasiDetailJadwal.route}/$idJadwal")
+                    println("PengelolaHalaman: idJadwal = $idJadwal")
+                },
                 onAddJadwal = {
                     navController.navigate(DestinasiInsertJadwal.route)
                 },
@@ -75,6 +82,51 @@ fun PengelolaHalaman(
                     navController.popBackStack()
                 },
                 modifier = modifier
+            )
+        }
+
+        composable (
+            DestinasiDetailJadwal.routeWithArg,
+            arguments = listOf(
+                navArgument(DestinasiDetailJadwal.IDJADWAL) {
+                    type = NavType.IntType
+                }
+            )
+        ){
+            val idJadwal = it.arguments?.getInt(DestinasiDetailJadwal.IDJADWAL)
+
+            idJadwal?.let { idJadwal ->
+                DetailJadwalView(
+                    onBack = {
+                        navController.popBackStack()
+                    },
+                    onEditClick = {
+                        navController.navigate("${DestinasiUpdateJadwal.route}/$idJadwal")
+                    },
+                    modifier = modifier,
+                    onDeleteClick = {
+                        navController.popBackStack()
+                    }
+                )
+            }
+        }
+
+        composable(
+            DestinasiUpdateJadwal.routeWithArg,
+            arguments = listOf(
+                navArgument (DestinasiUpdateJadwal.IDJADWAL) {
+                    type = NavType.IntType
+                }
+            )
+        ) {
+            UpdateJadwalView(
+                onBack = {
+                    navController.popBackStack()
+                },
+                onNavigate = {
+                    navController.popBackStack()
+                },
+                modifier = modifier,
             )
         }
     }
